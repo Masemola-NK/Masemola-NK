debb85993dc594ab6628bb0d10b0700ada869c87
Masemola-NK <fadeawwy445@gmail.com>
2025-06-05 20:29:19 +0200

    Ver-1 Datastructure

---
+++ src/MappingArrays.js
@@ -0,0 +1,11 @@
+// Array of objects
+const users = [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 30 },
+    { name: 'Charlie', age: 35 }
+];
+// Using map to create a new array of names
+const names = users.map(user => user.name);
+// Output the new array
+
+console.log(names); // ['Alice', 'Bob', 'Charlie']
